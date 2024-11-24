fdb2c85bff28a6f7684e60172e5befc9cb21e815
Kainic0804 <namkhanh0102@gmail.com>
2024-11-24 10:59:09 +0700

    Test

---
+++ Lab03/AimsProject/src/hust/soict/cybersec/aims/store/Store.java
@@ -0,0 +1,40 @@
+package hust.soict.cybersec.aims.store;
+
+import hust.soict.cybersec.aims.disc.DigitalVideoDisc;
+
+public class Store {
+    private DigitalVideoDisc[] itemInStore;
+    private int itemCount;
+
+    public Store() {
+        itemInStore = new DigitalVideoDisc[1000];
+        itemCount = 0;
+    }
+
+    public void addDVD(DigitalVideoDisc dvd) {
+        if (itemCount < itemInStore.length) {
+            itemInStore[itemCount] = dvd;
+            itemCount++;
+            System.out.println(dvd + " added to the store");
+        }
+        else {
+            System.out.println("The storage is full");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc dvd) {
+        for (int i = 0; i < itemCount; i++) {
+            if (itemInStore[i] == dvd) {
+                for (int j = i + 1; j < itemCount - 1; j++) {
+                    itemInStore[j] = itemInStore[j + 1];
+                }
+                itemInStore[itemCount - 1] = null;
+                itemCount--;
+                System.out.println(dvd + " removed from the store");
+                return;
+            }
+
+        }
+        System.out.println("Cannot find the item");
+    }
+}
