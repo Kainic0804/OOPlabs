6980cd47ea9bddc7a2077b8efae61a3bac9beb4e
Kainic0804 <namkhanh0102@gmail.com>
2024-11-24 11:35:28 +0700

    Lab

---
+++ Lab01/Seconddegreequation226.java
@@ -0,0 +1,34 @@
+package hust.soict.cybersec.lab01;
+
+import java.util.Scanner;
+
+public class Seconddegreequation226 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        double a = keyboard.nextDouble();
+        double b = keyboard.nextDouble();
+        double c = keyboard.nextDouble();
+        double x;
+
+        if(a==0){
+            x = -c/b;
+            System.out.println("PT co 1 nghiem x = " + x);
+        }
+        else{
+            double delta = b * b - 4 * a * c;
+            if(delta < 0){
+                System.out.println("PT vo nghiem");
+            }
+            if(delta == 0){
+                x = -b / 2 * a;
+                System.out.println("PT co 1 nghiem x = " + x);
+            }
+            if(delta > 0){
+                double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+                System.out.println("PT co 2 nghiem x1 = " + x1);
+                System.out.println("x2 = " + x2);
+            }
+        }
+    }
+}
---
+++ Lab03/AimsProject/src/hust/soict/cybersec/test/store/StoreTest.java
@@ -0,0 +1,23 @@
+package hust.soict.cybersec.test.store;
+
+import hust.soict.cybersec.aims.disc.DigitalVideoDisc;
+import hust.soict.cybersec.aims.store.Store;
+
+public class StoreTest {
+    public static void main(String[] args) {
+        Store store = new Store();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The lion King",
+                "Animation", "Rogger Aller", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation", 18.99f);
+
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+
+        store.removeDVD(dvd1);
+        store.removeDVD(dvd3);
+    }
+}
