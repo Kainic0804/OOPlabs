d4a201e62054ecd7e058752c310a203627987756
Kainic0804 <namkhanh0102@gmail.com>
2024-11-24 12:47:47 +0700

    Lab3

---
+++ Lab03/OtherProjects/hust/soict/cybersec/lab01/JavaBasic61.java
@@ -0,0 +1,14 @@
+package hust.soict.cybersec.lab01;
+
+import javax.swing.JOptionPane;
+
+public class JavaBasic61 {
+    public static void main(String[] args) {
+        int option = JOptionPane.showConfirmDialog(null,
+                    "Do want to change your first class ticket?");
+
+        JOptionPane.showMessageDialog(null, "You've chosen: "
+                                      + (option == JOptionPane.YES_NO_CANCEL_OPTION ? "Yes" : "No" ));
+        System.exit(0);
+    }
+}
